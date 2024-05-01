0d31aae0d2ec5b8a37e1285533dbd4da08de3ccc
Jason Nguyen <nguyen.j1305@gmail.com>
2024-05-01 02:05:36 -0700

    temp

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="name">
+            Jason Nguyen
+        </div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">
+            Connect With Me
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
