aef0aad60f364d05ecff1e9e8dd7a2274b6844c6
Jason Nguyen <nguyen.j1305@gmail.com>
2024-05-01 14:36:36 -0700

    basic features

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../assets/profile_img.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img} alt=""></img>
+        <h1>
+          <span>I'm Jason Nguyen,</span> a computer science graduate based in Canada
+        </h1>
+        <p>I received my Bachelor of Science at UBC majoring in Computer Science and Minoring in Statistics</p>
+        <div className = "hero-action">
+          <div className="hero-connect">
+            Connect with me
+          </div>
+          <div className="hero-resume">
+            My resume
+          </div>
+        </div>
     </div>
   )
 }
